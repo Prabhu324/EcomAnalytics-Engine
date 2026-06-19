c3560c227e4c14251bc282f55d962da0ed463bfe
Prabhu324 <prabhupada.d2004@gmail.com>
2026-06-19 19:53:03 +0530

    Backend Template added

---
+++ SpringBoot_BackEnd/SpringBoot_Backend/src/test/java/com/EcomAnalytics_Engine/SpringBoot_Backend/SpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EcomAnalytics_Engine.SpringBoot_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
